6a80a63ae700e5142a5ce404f493280d56a43fa7
Jack O'Connor <oconnor663@gmail.com>
2021-10-26 17:20:08 -0400

    pure Python XSalsa20

--- salsa20.py
+++ salsa20.py
@@ -88,6 +88,7 @@ def salsa20_stream(key, nonce, length):
 
 
 def test_salsa20():
+    # Test this implementation against `pip install pycryptodome`.
     from Crypto.Cipher import Salsa20 as PyCryptodomeSalsa20
     import secrets
 
@@ -97,3 +98,57 @@ def test_salsa20():
     here = salsa20_stream(key, nonce, length)
     there = PyCryptodomeSalsa20.new(key, nonce).encrypt(b"\0" * length)
     assert here == there
+
+
+def hsalsa20(key, input_bytes):
+    # This implementation doesn't support 16-byte keys.
+    assert len(key) == 32
+    assert len(input_bytes) == 16
+    constant_words = words_from_bytes(b"expand 32-byte k")
+    key_words = words_from_bytes(key)
+    input_words = words_from_bytes(input_bytes)
+    block = [
+        constant_words[0],
+        key_words[0],
+        key_words[1],
+        key_words[2],
+        key_words[3],
+        constant_words[1],
+        input_words[0],
+        input_words[1],
+        input_words[2],
+        input_words[3],
+        constant_words[2],
+        key_words[4],
+        key_words[5],
+        key_words[6],
+        key_words[7],
+        constant_words[3],
+    ]
+    salsa20_permute(block)
+    outputs = [block[i] for i in [0, 5, 10, 15, 6, 7, 8, 9]]
+    return bytes_from_words(outputs)
+
+
+def xsalsa20_stream(key, nonce, length):
+    # This implementation doesn't support 16-byte keys.
+    assert len(key) == 32
+    assert len(nonce) == 24
+    derived_key = hsalsa20(key, nonce[:16])
+    return salsa20_stream(derived_key, nonce[16:], length)
+
+
+def test_xsalsa20():
+    # Test this implementation against `pip install pynacl`.
+    from nacl.secret import SecretBox
+    import secrets
+
+    key = secrets.token_bytes(32)
+    nonce = secrets.token_bytes(24)
+    length = 1000
+    # Account for the fact that secretbox uses the first 32 bytes of stream
+    # output for the Poly1305 key.
+    here = xsalsa20_stream(key, nonce, length + 32)
+    there = SecretBox(key).encrypt(b"\0" * length, nonce).ciphertext
+    assert len(there) == length + 16
+    assert here[32:] == there[16:]
